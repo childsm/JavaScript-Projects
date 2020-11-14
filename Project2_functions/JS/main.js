4f373c629441f1381c4e9a35d9554e566fe1a8ad
childsm <73154214+childsm@users.noreply.github.com>
2020-11-13 23:33:25 -0800

    New Item

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,14 @@
+function mouseAlert() { //for step 60 find a new HTML event and use
+    alert("The only way to land. On water!"); }
+
+function stringFunction() { //for step 61 use the getElementById method
+    var str = "Purple String";
+    var result = str.fontcolor("purple");
+    document.getElementById("purpleWords").innerHTML = result;
+}
+
+function concatF() { //for step 63 use the += operator
+    var first = "Now it is time ";
+    first += "to go to sleep";
+    document.getElementById("concat").innerHTML = first;
+}
